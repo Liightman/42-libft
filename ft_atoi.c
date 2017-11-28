cdc0fb3660188a6e58d86f3ee5fdea245985518a
Llyam GARCIA <lgarcia@e2r10p19.42.fr>
2017-11-28 12:05:08 +0100

    Add ended funcs

---
+++ ft_atoi.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	total;
+	int	sign;
+
+	sign = 1;
+	total = 0;
+	while (ft_isspace(*str))
+		++str;
+	if (!ft_isdigit(*str))
+	{
+		if (*str == '-')
+			sign = -1;
+		else if (*str != '+')
+			return (0);
+		str++;
+	}
+	while (ft_isdigit(*str))
+		total = total * 10 + (*str++ - '0') * sign;
+	return (total);
+}
